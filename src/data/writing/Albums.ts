eeec238e67be390df62ad2fe58ae059097b4a3b4
Jeremy Costello <jeremy.costello@outlook.com>
2025-07-16 16:10:22 -0300

    add more albums

--- src/data/writing/Albums.ts
+++ src/data/writing/Albums.ts
@@ -6,8 +6,21 @@ export const albums = {
     preamble: "These are some of my favourite albums, mostly in no particular order.",
     items: [
       {
-        title: "Deathconsciousness",
+        title: "Deathconsciousness - Have a Nice Life",
         image: `${BASE_PATH}/images/albums/deathconsciousness.jpg`,
+        link: "https://en.wikipedia.org/wiki/Deathconsciousness",
+        align: LEFT_ALIGN
+      },
+      {
+        title: "Spiderland - Slint",
+        image: `${BASE_PATH}/images/albums/spiderland.jpg`,
+        link: "https://en.wikipedia.org/wiki/Spiderland",
+        align: LEFT_ALIGN
+      },
+      {
+        title: "Norman Fucking Rockwell! - Lana Del Rey",
+        link: "https://en.wikipedia.org/wiki/Norman_Fucking_Rockwell!",
+        image: `${BASE_PATH}/images/albums/nfr.png`,
         align: LEFT_ALIGN
       }
     ]
